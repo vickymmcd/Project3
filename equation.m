2d351ac148fc9d491f6f7c8164a2a79268128071
vickymmcd <vickymmcd@gmail.com>
2016-11-14 16:27:25 -0500

    added equation file

---
+++ equation.m
@@ -0,0 +1,17 @@
+function res = equation(~,M)
+    %set given variables
+    m=70; %kg
+    l=.1; %m
+    g=9.8; %m/s^2
+    
+    %unpack current position and velocity
+    theta = M(1);
+    dtheta = M(2);
+    
+    %solve for ddtheta
+    ddtheta = -(g*sin(theta))/l;
+    
+    %store new positions/velocities in res
+    res = [dtheta, ddtheta];
+    
+end
