52c3826cffb66387d0e453ef07d88b1e62a5af79
vickymmcd <vickymmcd@gmail.com>
2016-11-15 16:34:46 -0500

    comet like animation single pendulum

--- pendulum.m
+++ pendulum.m
@@ -1,15 +1,24 @@
-function pendulum()
+function [X, Y] = pendulum()
     %set up initial conditions
     G = [1.22, .01];
     endTime = 2;
+    l = .1;
     
     [t,S] = ode45(@equation, [0,endTime], G);
     %unpack the solution
-    theta = S(:,1);    
+    theta = S(:,1);   
+    dtheta = S(:,2);    
+
+    x = l.*sin(theta);
+    y = -l.*cos(theta);
     
-    plot(t, theta, 'r-');
+    comet(x,y);
+    
+    %plot(t, theta, 'r-');
     xlabel('Time');
     ylabel('Theta');
     title('Pendulum Position over time');
+    X = x;
+    Y = y;
     
 end
