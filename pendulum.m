3bc0e56e71bfadf7aa6a3b61ceec47f5a7c149fb
vickymmcd <vickymmcd@gmail.com>
2016-11-23 15:48:42 -0500

    got pendulum animation working

--- pendulum.m
+++ pendulum.m
@@ -1,7 +1,7 @@
 function [X, Y] = pendulum()
     %set up initial conditions
     G = [pi/2, pi/2, 0, 0];
-    endTime = 5;
+    endTime = 15;
     l = .1;
     
     [t,S] = ode45(@equation, [0,endTime], G);
@@ -16,18 +16,18 @@ function [X, Y] = pendulum()
     x2 = l.*sin(theta1) + l.*sin(theta2);
     y2 = -l.*cos(theta1)- l.*cos(theta2);
     
-    hold on;
+%     hold on;
 %     plot(t, theta1, 'r-');
 %     plot(t, theta2, 'b-');
-    comet(x1, y1);
-    comet(x2, y2);
+%      comet(x1, y1);
+%      comet(x2, y2);
 
     
     %plot(t, theta, 'r-');
     xlabel('Time');
     ylabel('Theta');
     title('Pendulum Position over time');
-    X = x1;
-    Y = y1;
+    X = t;
+    Y = S;
     
 end
---
+++ pendulumAnimation.m
@@ -0,0 +1,35 @@
+function pendulumAnimation()
+
+    function animate_func(T,M)
+        % animate the positions of the planets, assuming that the
+        % columns of M are x1, y1, x2, y2.
+        theta1 = M(:,1);   
+        theta2 = M(:,2);
+
+        l=.1;
+        X1 = l.*sin(theta1);
+        Y1 = -l.*cos(theta1);
+        X2 = l.*sin(theta1) + l.*sin(theta2);
+        Y2 = -l.*cos(theta1)- l.*cos(theta2);
+        
+        minmax = [min([X1;X2]), max([X1;X2]), min([Y1;Y2]), max([Y1;Y2])];
+        for i=1:length(T)
+            clf;
+            axis(minmax);
+            hold on;
+            draw_func(X1(i), Y1(i), X2(i), Y2(i));
+            drawnow;
+        end
+    end
+
+    function draw_func(x1, y1, x2, y2)
+        plot(x1, y1, 'r.', 'MarkerSize', 50);
+        plot(x2, y2, 'b.', 'MarkerSize', 50);
+        plot([0 x1], [0 y1]);
+        plot([x1 x2], [y1 y2]);
+    end
+    
+    [T,W] = pendulum();
+    animate_func(T,W);
+    
+end
