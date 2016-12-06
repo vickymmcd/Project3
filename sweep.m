af132bce94d9caed001536fce9af78ebed7d91e1
vickymmcd <vickymmcd@gmail.com>
2016-12-06 14:54:56 -0500

    made better punchline

--- sweep.m
+++ sweep.m
@@ -1,9 +1,9 @@
 function sweep()
 hold on;
-ylabel('Initial Theta 1 (radians)');
-xlabel('Mass of Top Rider (kg)');
+ylabel('Initial Theta 1 (degrees)');
+xlabel('Mass of Top Rider (lbs)');
 c = colorbar;
-ylabel(c,'Y1-Y2 (meters)');
+ylabel(c,'Y1-Y2 (cm)');
 title('The Swing');
 m2 = 30;
 L = zeros(10,10);
@@ -13,7 +13,8 @@ for i=1:1:10
     itheta1=(i*pi)/20;
     %itheta12(1,i) = itheta1;
     for s=1:1:10
-        m1 =20+(10*s);
+        sy = s-1;
+        m1 =(10*sy);
         G  = [itheta1,0,0,0];
         [t,y1,y2] = pendulum(G,m1,m2);
         L(s,i) = min(y1-y2);
@@ -22,7 +23,14 @@ for i=1:1:10
     end
 end 
 
+m12 = m12*2.2;
+itheta12 = (itheta12*180)/pi;
+L = L*100;
 
-contourf(m12, itheta12, L);
+% Then create our beautiful plot.
+pcolor(m12, itheta12, L)
+shading interp
+colorbar
 
+contour(m12, itheta12, L, [0 0], 'r*-', 'LineWidth', 3)
 end
